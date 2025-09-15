9aa05f60ba6885e604a631fdee6db8cc4e4c984b
NightDev19 <sherwintajan143@gmail.com>
2025-09-15 11:56:17 +0800

    Vue with Shacdn

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [vue(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
