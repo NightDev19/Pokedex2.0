5e1cfc7ce2174631b4401ec9c9a105cdbdc264e5
NightDev19 <sherwintajan143@gmail.com>
2025-09-15 11:42:32 +0800

    Restarting all

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router"; // 👈 import your router
-
-const app = createApp(App);
-
-app.use(router); // 👈 register router
-app.mount("#app");
