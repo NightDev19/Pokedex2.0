7cb46849ca3770355e67d2708640e8fa96cf1fc0
NightDev19 <sherwintajan143@gmail.com>
2025-10-04 16:53:44 +0800

    Feat (Dev) : Adding runtime logging using vite-plugin-checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
+import checker from "vite-plugin-checker";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    checker({
+      vueTsc: true,
+      typescript: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
